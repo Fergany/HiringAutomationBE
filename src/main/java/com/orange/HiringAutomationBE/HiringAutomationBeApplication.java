e19eac3b4beb16e8d59e3704720a11dc46cb3dda
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-11 15:29:11 +0200

    create new project

---
+++ src/main/java/com/orange/HiringAutomationBE/HiringAutomationBeApplication.java
@@ -0,0 +1,13 @@
+package com.orange.HiringAutomationBE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiringAutomationBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiringAutomationBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orange/HiringAutomationBE/HiringAutomationBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orange.HiringAutomationBE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiringAutomationBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
