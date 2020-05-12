b90d17de4b6aecb9b4c2be5b1df9fe2918c77426
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-12 16:05:43 +0200

    edit in Main app

--- src/main/java/com/orange/hiring_automation/HiringAutomationApplication.java
+++ src/main/java/com/orange/hiring_automation/HiringAutomationApplication.java
@@ -1,13 +1,18 @@
-package com.orange.HiringAutomation;
+package com.orange.hiring_automation;
 
+import com.orange.hiring_automation.model.Job;
+import com.orange.hiring_automation.repository.JobRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 public class HiringAutomationApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(HiringAutomationApplication.class, args);
-	}
-
+        SpringApplication.run(HiringAutomationApplication.class, args);
+    }
 }
--- src/test/java/com/orange/hiring_automation/HiringAutomationBeApplicationTests.java
+++ src/test/java/com/orange/hiring_automation/HiringAutomationBeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.orange.HiringAutomation;
+package com.orange.hiring_automation;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
