de5c8e441aead0ba9c8bab96ee892fa2b9e91c49
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-12 11:59:55 +0200

    add Model Job

---
+++ src/main/java/com/orange/hiring_automation/model/Job.java
@@ -0,0 +1,54 @@
+package com.orange.hiring_automation.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "jobs")
+public class Job {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "job_id")
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "required_skills")
+    private String requiredSkills;
+
+    @Override
+    public String toString() {
+        return String.format(
+                "Job[id=%d, title='%s', description='%s, required_skills=%s']",
+                id, title, description, requiredSkills);
+    }
+
+    public Job(){
+
+    }
+
+    public Job(String title, String description, String requiredSkills){
+        this.title = title;
+        this.description = description;
+        this.requiredSkills = requiredSkills;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getRequiredSkills() {
+        return requiredSkills;
+    }
+}
