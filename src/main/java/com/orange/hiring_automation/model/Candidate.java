1a7cc52aeaacf8fdeb8b401661a6ab10c4094ebe
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-14 14:57:23 +0200

    add Candidate model

---
+++ src/main/java/com/orange/hiring_automation/model/Candidate.java
@@ -0,0 +1,27 @@
+package com.orange.hiring_automation.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "candidates")
+public class Candidate {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "candidate_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "attached_cv")
+    private String attachedCV;
+}
