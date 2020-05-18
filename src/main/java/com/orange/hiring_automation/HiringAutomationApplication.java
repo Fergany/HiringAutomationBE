b728031bd625c597ed49e77b327b00afb7866e49
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-05-18 14:33:18 +0200

    add @EnableJpaAuditing

--- src/main/java/com/orange/hiring_automation/HiringAutomationApplication.java
+++ src/main/java/com/orange/hiring_automation/HiringAutomationApplication.java
@@ -1,16 +1,12 @@
 package com.orange.hiring_automation;
 
-import com.orange.hiring_automation.model.Job;
-import com.orange.hiring_automation.repository.JobRepository;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HiringAutomationApplication {
 	public static void main(String[] args) {
         SpringApplication.run(HiringAutomationApplication.class, args);
