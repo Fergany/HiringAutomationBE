0c156e20583b06bc018b763d434df9744f3d4f78
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-06-22 09:59:47 +0200

    add JobExam

---
+++ src/main/java/com/orange/hiring_automation/model/JobExam.java
@@ -0,0 +1,23 @@
+package com.orange.hiring_automation.model;
+
+import lombok.Data;
+import org.hibernate.annotations.ManyToAny;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "job_exams")
+public class JobExam {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "job_id")
+    private Job job;
+
+    @ManyToOne
+    @JoinColumn(name = "exam_id")
+    private Exam exam;
+}
