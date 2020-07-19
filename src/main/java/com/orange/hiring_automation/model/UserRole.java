8843f0da055fd539315932a5a42305a3780f0bbe
FERGANY Hazem Ext O-EG/HRCS <lnpx8002@orange.com>
2020-07-19 13:02:26 +0200

    add UserRole model

---
+++ src/main/java/com/orange/hiring_automation/model/UserRole.java
@@ -0,0 +1,19 @@
+package com.orange.hiring_automation.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "user_roles")
+public class UserRole {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "user_role_id")
+    private Long id;
+
+    @Column(name = "role")
+    private String role;
+}
+
